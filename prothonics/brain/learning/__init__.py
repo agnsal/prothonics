98c3f7086e60e1052dfc3e120bd91475b6ae0b6b
Agnese Salutari <agneses92@hotmail.it>
2020-01-29 16:46:23 +0100

    Update __init__.py

--- prothonics/brain/learning/__init__.py
+++ prothonics/brain/learning/__init__.py
@@ -1,7 +1,7 @@
 # coding : utf-8
 
 '''
-Copyright 2019 Agnese Salutari.
+Copyright 2019-2020 Agnese Salutari.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
